507a962d7adf286c5033c74227f64f7fa29e23b5
RYAN JAMES <realcoder2023@gmail.com>
2025-12-05 13:04:22 -0500

    added sets to repo

---
+++ Control Flow/ControlFlow.js
@@ -0,0 +1,24 @@
+let a = 26;
+
+if (a === 26) {
+
+    console.log("Old asf");
+}
+else {
+    console.log("Your Not Old Enough")
+}
+
+let bool = Boolean(false)
+
+switch(a) {
+
+    case 26:
+        console.log("26")
+        break;
+}
+
+while(!bool) {
+    a += 1;
+    bool = true;
+    console.log(bool);
+}

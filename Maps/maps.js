4b50f7ddca8a2b25c4738b94125f1cc3d9625040
RYAN JAMES <realcoder2023@gmail.com>
2025-12-04 19:13:17 -0500

    added and learned about maps

---
+++ Maps/maps.js
@@ -0,0 +1,11 @@
+const mapsFunction = async () => {
+  const map = new Map();
+  map.set(0, "RealCodingRJ");
+  map.set(1, "CodingGarden");
+
+  const mapValues = map.values();
+
+  for (const maps of mapValues) {
+    console.log(maps);
+  }
+};
